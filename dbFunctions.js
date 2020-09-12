704d53ceaee719cb3d8f00ac5b4328f08deea631
Paul Varner <fuzzyray@gmail.com>
2020-09-12 18:41:53 -0500

    Add createIssue functionality

--- dbFunctions.js
+++ dbFunctions.js
@@ -28,13 +28,13 @@ const IssueSchema = new mongoose.Schema({
 const Issue = mongoose.model('Issue', IssueSchema);
 
 // Create
-const createOrFindProject = (name, cb) => {
+const findOrCreateProject = (name, cb) => {
   const newProject = new Project({name: name});
-  findProjectByName(name, (err, data) => {
+  Project.find({name: name}, (err, data) => {
     if (err) {
       cb(err, null);
     } else if (data) {
-      cb(null, data);
+      cb(null, data[0]);
     } else {
       newProject.save((err, data) => {
         if (err) {
@@ -47,20 +47,39 @@ const createOrFindProject = (name, cb) => {
   });
 };
 
-// Read
-const findProjectByName = (name, cb) => {
-  Project.find({name: name}, (err, data) => {
+const createIssue = (issue, cb) => {
+  /*
+  {
+    project: {type: mongoose.Types.ObjectId, ref: 'Project', required: true},
+    issue_title: {type: String, required: true},
+    issue_text: {type: String, required: true},
+    created_on: {type: Date, required: true},
+    created_by: {type: String, required: true},
+    updated_on: {type: Date, required: true},
+    open: {type: Boolean, default: true, required: true},
+    assigned_to: {type: String},
+    status_text: {type: String},
+  }
+  */
+  issue.created_on = new Date();
+  issue.updated_on = issue.created_on;
+  // TODO: Add checking for required fields? We get an error from db, if missing
+  const newIssue = new Issue(issue);
+  newIssue.save((err, data) => {
     if (err) {
       cb(err, null);
     } else {
-      cb(null, data[0]);
+      cb(null, data);
     }
   });
 };
+
+// Read
 
 // Update
 
 // Delete
 
 exports.connect = connect;
-exports.createOrFindProject = createOrFindProject;
+exports.findOrCreateProject = findOrCreateProject;
+exports.createIssue = createIssue;
--- routes/api.js
+++ routes/api.js
@@ -9,55 +9,75 @@
 'use strict';
 
 const expect = require('chai').expect;
-const createOrFindProject = require('../dbFunctions').createOrFindProject;
+const findOrCreateProject = require('../dbFunctions').findOrCreateProject;
+const createIssue = require('../dbFunctions').createIssue;
+
+const defaultIssue = {
+  '_id': '0',
+  'issue_title': 'No Issues in Project',
+  'issue_text': 'This is the default issue returned, if no issues are found.',
+  'created_on': '1970-01-01T00:00:00.000Z',
+  'updated_on': '1970-01-01T00:00:00.000Z',
+  'created_by': 'System',
+  'assigned_to': 'System',
+  'open': true,
+  'status_text': 'Bug',
+};
+
+const createResult = (data) => {
+  return {
+    _id: data._id,
+    issue_title: data.issue_title,
+    issue_text: data.issue_text,
+    created_on: data.created_on.toISOString(),
+    updated_on: data.updated_on.toISOString(),
+    created_by: data.created_by,
+    assigned_to: data.assigned_to,
+    open: data.open,
+    status_text: data.status_text,
+  };
+};
 
 module.exports = app => {
-
-  const defaultIssue = {
-    '_id': '0',
-    'issue_title': 'No Issues in Project',
-    'issue_text': 'This is the default issue returned, if no issues are found.',
-    'created_on': '1970-01-01T00:00:00.000Z',
-    'updated_on': '1970-01-01T00:00:00.000Z',
-    'created_by': 'System',
-    'assigned_to': 'System',
-    'open': true,
-    'status_text': 'Bug',
-  };
 
   app.route('/api/issues/:project')
 
       .get((req, res) => {
-        const project = req.params.project;
+        const project = req.params.project.toLowerCase();
         console.log(project);
         res.json([defaultIssue]);
       })
 
       .post((req, res) => {
-        const project = req.params.project;
+        const project = req.params.project.toLowerCase();
         const issue = req.body;
-        createOrFindProject(project, (err, data) => {
+        findOrCreateProject(project, (err, data) => {
           if (err) {
-            console.log(err);
+            console.error(err);
             res.json({error: err});
           } else {
-            console.log(data);
-            issue.projectId = data._id;
-            console.log(issue);
-            res.json({error: 'Not implemented'});
+            issue.project = data._id;
+            createIssue(issue, (err, data) => {
+              if (err) {
+                console.error(err);
+                res.json({error: err});
+              } else {
+                res.json(createResult(data));
+              }
+            });
           }
         });
       })
 
       .put((req, res) => {
-        const project = req.params.project;
+        const project = req.params.project.toLowerCase();
         console.log(project);
         console.log(req.body);
         res.json({error: 'Not implemented'});
       })
 
       .delete((req, res) => {
-        const project = req.params.project;
+        const project = req.params.project.toLowerCase();
         console.log(project);
         console.log(req.body);
         res.json({error: 'Not implemented'});
